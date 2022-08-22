09d8a0774639209ab28043743ba1d1bc973f8024
ThugRaven <kamilo1715@wp.pl>
2022-08-22 19:57:59 +0200

    feat: update NextPageWithLayout type

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,27 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { NextPage } from 'next';
 import type { AppProps } from 'next/app';
+import { ReactElement, ReactNode } from 'react';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  // eslint-disable-next-line no-unused-vars
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+  // return (
+  //   <ChakraProvider>
+  //     <Component {...pageProps} />
+  //   </ChakraProvider>
+  // );
+
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <ChakraProvider>{getLayout(<Component {...pageProps} />)}</ChakraProvider>
   );
 }
-
-export default MyApp;
