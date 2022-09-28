9d95d913639604da4225e384379d47e1590dab75
ThugRaven <kamilo1715@wp.pl>
2022-09-28 17:27:35 +0200

    feat: remove template from index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import { Button, useColorMode } from '@chakra-ui/react';
-import Image from 'next/image';
 import { ReactElement } from 'react';
 import { MainLayout } from '../components/layouts';
 import s from '../styles/Home.module.css';
@@ -11,61 +10,14 @@ const Home: NextPageWithLayout = () => {
   return (
     <div className={s.container}>
       <main className={s.main}>
-        <h1 className={s.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <Button onClick={toggleColorMode}>
-          Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
-        </Button>
+        <h1 className={s.title}>Strona główna</h1>
 
         <p className={s.description}>
-          Get started by editing <code className={s.code}>pages/index.tsx</code>
+          <Button onClick={toggleColorMode}>
+            Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
+          </Button>
         </p>
-
-        <div className={s.grid}>
-          <a href="https://nextjs.org/docs" className={s.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={s.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={s.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={s.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
-
-      <footer className={s.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={s.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 };
