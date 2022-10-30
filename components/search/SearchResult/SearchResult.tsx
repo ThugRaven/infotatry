5b549030c060513c5c88987bae73834318cd3b50
ThugRaven <kamilo1715@wp.pl>
2022-10-30 22:10:50 +0100

    feat: add `SearchResult`

---
+++ components/search/SearchResult/SearchResult.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from 'react';
+import s from './SearchResult.module.css';
+
+interface SearchResultProps {
+  id: number;
+  name: string;
+  type: 'node' | 'trail';
+  icon: ReactNode;
+  onClick: (type: 'node' | 'trail', id: number) => void;
+}
+
+const SearchResult = ({ id, name, type, icon, onClick }: SearchResultProps) => {
+  const handleClick = () => {
+    onClick(type, id);
+  };
+
+  return (
+    <li key={`${type}-${id}`}>
+      <a className={s.search__result} onClick={handleClick}>
+        <div>{icon}</div>
+        <span>{name}</span>
+      </a>
+    </li>
+  );
+};
+
+export default SearchResult;
