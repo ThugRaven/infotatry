c45bebef0d9d23c760f370d09fde11b6de0efc31
ThugRaven <kamilo1715@wp.pl>
2022-11-14 23:31:55 +0100

    refactor: add types to `Graph`

--- lib/Graph.ts
+++ lib/Graph.ts
@@ -5,14 +5,14 @@ export type Edge = {
 };
 
 export default class Graph {
-  adjacencyList = new Map();
+  adjacencyList = new Map<number, Edge[]>();
 
   addVertex(node: number) {
     this.adjacencyList.set(node, []);
   }
 
   addEdge(origin: Edge, destination: Edge) {
-    this.adjacencyList.get(origin.node_id).push(destination);
-    this.adjacencyList.get(destination.node_id).push(origin);
+    this.adjacencyList.get(origin.node_id)?.push(destination);
+    this.adjacencyList.get(destination.node_id)?.push(origin);
   }
 }
