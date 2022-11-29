da43dc9a6c1ff46712d6071880c33b970bb6597e
ThugRaven <kamilo1715@wp.pl>
2022-11-29 23:55:41 +0100

    refactor: update constants exports

---
+++ constants/constants.ts
@@ -0,0 +1,11 @@
+export const ENCODING_PRECISION = 6;
+
+export const TRAIL_COLORS = {
+  RED: '#FF0000',
+  BLUE: '#0095DA',
+  YELLOW: '#FFCB04',
+  GREEN: '#00A650',
+  BLACK: '#000000',
+} as const;
+
+export const INTERACTIVE_LAYER_IDS = ['trails-data-layer', 'nodes-draw-layer'];
--- constants/index.ts
+++ constants/index.ts
@@ -1,9 +1,2 @@
-export const ENCODING_PRECISION = 6;
-
-export const TRAIL_COLORS = {
-  RED: '#FF0000',
-  BLUE: '#0095DA',
-  YELLOW: '#FFCB04',
-  GREEN: '#00A650',
-  BLACK: '#000000',
-} as const;
+import * as constants from './constants';
+export { constants };
