538026de72ad5ea2754a85dba90fbbc73231370c
ThugRaven <kamilo1715@wp.pl>
2022-12-05 21:21:51 +0100

    feat: change trail color

--- constants/constants.ts
+++ constants/constants.ts
@@ -3,8 +3,8 @@ export const ENCODING_PRECISION = 6;
 export const TRAIL_COLORS = {
   RED: '#FF0000',
   BLUE: '#0095DA',
-  // YELLOW: '#FFE066',
-  YELLOW: '#FFCB04',
+  YELLOW: '#FFE066',
+  // YELLOW: '#FFCB04',
   GREEN: '#00A650',
   BLACK: '#000000',
 } as const;
