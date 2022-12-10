159cdc3baee5b36f16d95e2b2a56a58cf37382a4
ThugRaven <kamilo1715@wp.pl>
2022-12-10 18:46:31 +0100

    feat: add test fetch

--- components/map/MapSidebar/MapSidebar.tsx
+++ components/map/MapSidebar/MapSidebar.tsx
@@ -1,5 +1,5 @@
 import { ChevronLeftIcon, SearchIcon } from '@chakra-ui/icons';
-import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import { Button, Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
 import { useEffect, useRef } from 'react';
 import s from './MapSidebar.module.css';
 
@@ -17,6 +17,23 @@ const MapSidebar = ({ isOpen, onToggle, onWidthChange }: MapSidebarProps) => {
     onWidthChange(ref.current.offsetWidth);
   }, []);
 
+  const handleRouteSearch = async () => {
+    try {
+      const response = await fetch(
+        'http://localhost:8080/route/Palenica Białczańska;Rówień Waksmundzka',
+        {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json' },
+        },
+      );
+
+      console.log(response);
+      console.log(response.json().then((data) => console.log(data)));
+    } catch (error) {
+      console.log('error');
+    }
+  };
+
   return (
     <div className={s.wrapper}>
       <div className={`${s.container} ${!isOpen ? s.collapsed : ''}`} ref={ref}>
@@ -31,6 +48,9 @@ const MapSidebar = ({ isOpen, onToggle, onWidthChange }: MapSidebarProps) => {
           <InputLeftElement pointerEvents="none" children={<SearchIcon />} />
           <Input type="text" placeholder="Szukaj..." />
         </InputGroup>
+        <Button mt={2} onClick={handleRouteSearch}>
+          Search
+        </Button>
       </div>
     </div>
   );
