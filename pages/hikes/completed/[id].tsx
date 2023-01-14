f4e0457f42fb439826a70ffbd943b978472ba1a4
ThugRaven <kamilo1715@wp.pl>
2023-01-14 20:09:40 +0100

    feat: add server side rendering for completed hike

--- pages/hikes/completed/[id].tsx
+++ pages/hikes/completed/[id].tsx
@@ -2,84 +2,62 @@ import { MainLayout } from '@components/layouts';
 import { MapContainer } from '@components/map';
 import s from '@styles/Hikes.module.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import { useRouter } from 'next/router';
-import { Node, Trail } from 'pages/dashboard/admin/map';
-import { ReactElement, useEffect, useState } from 'react';
-import { useQuery } from 'react-query';
-import features from '../../../public/features.json';
+import { GetServerSideProps } from 'next';
+import { ReactElement } from 'react';
 
-const CompletedHike = () => {
-  const [trails, setTrails] = useState<Trail[]>([]);
-  const [nodes, setNodes] = useState<Node[]>([]);
-  const [routeNodes, setRouteNodes] = useState<Node[]>([]);
+export const getServerSideProps: GetServerSideProps<{ hike: any }> = async (
+  context,
+) => {
+  const { id } = context.query;
 
-  useEffect(() => {
-    const data = features;
-    setTrails(data.trails as Trail[]);
-    setNodes(data.nodes as Node[]);
-  }, []);
+  console.log(id);
+  console.log(context.req.cookies);
+  const authCookie = context.req.cookies['connect.sid'];
+  let hike = null;
 
-  const router = useRouter();
-  const { id } = router.query;
+  try {
+    console.log('fetch');
+    console.log(id);
 
-  // useEffect(() => {
-  //   console.log('useEffect');
-  //   if (id && typeof id === 'string' && nodes) {
-  //     const decoded = decodeURIComponent(id);
-  //     const idsArray = decoded.split(',');
-  //     console.log(idsArray);
-  //     const route: Node[] = [];
-  //     idsArray.forEach((id) => {
-  //       const node = nodes.find((node) => node.id === parseInt(id));
-  //       if (node) {
-  //         console.log(node);
-  //         route.push(node);
-  //       }
-  //     });
-  //     setRouteNodes(route);
-  //   }
-  // }, [id, nodes]);
-
-  const fetchHike = async (id: string | string[] | undefined) => {
-    try {
-      console.log('fetch');
-      console.log(id);
-
-      const response = await fetch(
-        `http://localhost:8080/hikes/completed/${id}`,
-        {
-          method: 'GET',
-          headers: { 'Content-Type': 'application/json' },
-          credentials: 'include',
+    const response = await fetch(
+      `http://localhost:8080/hikes/completed/${id}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Cookie: `connect.sid=${authCookie};`,
         },
-      );
-
-      if (!response.ok) {
-        const data = await response.json();
-        throw new Error(data.message);
-      }
-
-      return response.json();
-    } catch (error) {
-      console.log(error);
-      if (error instanceof Error) {
-        throw new Error(error.message);
-      }
-    }
-  };
-
-  const { isLoading, error, data } = useQuery<any, Error>(
-    ['route', id],
-    () => fetchHike(id),
-    {
-      enabled: Boolean(id),
-      refetchOnWindowFocus: false,
-      retry: false,
-      onSuccess: (data) => {
-        console.log(data);
+        credentials: 'include',
       },
-    },
-  );
+    );
+
+    if (!response.ok) {
+      throw new Error(response.status.toString());
+    }
+
+    hike = await response.json();
+  } catch (error) {
+    console.log(error);
+    if (error instanceof Error) {
+      if (error.message == '401') {
+        return {
+          redirect: {
+            destination: '/login',
+            permanent: false,
+          },
+        };
+      }
+      throw new Error(error.message);
+    }
+  }
+
+  return {
+    props: { hike },
+  };
+};
+
+const CompletedHike = ({ hike }: any) => {
+  console.log('getServerSideProps', hike);
 
   return (
     <div className={s.container}>
@@ -91,42 +69,41 @@ const CompletedHike = () => {
         ))}
       </ul> */}
 
-      {data && (
+      {hike && (
         <>
           <h1
             className={s.hike__names}
-          >{`${data.name.start} - ${data.name.end}`}</h1>
+          >{`${hike.name.start} - ${hike.name.end}`}</h1>
           <ul className={s.hike__info}>
-            <li title={`${data.distance} m`}>
+            <li title={`${hike.distance} m`}>
               <span>Distance</span>
               {`${
-                (Math.floor(data.distance / 1000) * 1000 +
-                  Math.round((data.distance % 1000) / 100) * 100) /
+                (Math.floor(hike.distance / 1000) * 1000 +
+                  Math.round((hike.distance % 1000) / 100) * 100) /
                 1000
               } km`}
             </li>
-            <li title={`${data.time} min.`}>
+            <li title={`${hike.time} min.`}>
               <span>Time</span>
-              {`${Math.floor(data.time / 60)}:${
-                data.time % 60 >= 10 ? data.time % 60 : `0${data.time % 60}`
+              {`${Math.floor(hike.time / 60)}:${
+                hike.time % 60 >= 10 ? hike.time % 60 : `0${hike.time % 60}`
               } h`}
             </li>
             <li>
               <span>Ascent</span>
-              {`${data.ascent} m`}
+              {`${hike.ascent} m`}
             </li>
             <li>
               <span>Descent</span>
-              {`${data.descent} m`}
+              {`${hike.descent} m`}
             </li>
           </ul>
         </>
       )}
 
       <MapContainer
-        trailIds={data && data.encoded != '' ? data.trails : null}
-        hike={data && data.encoded == '' ? null : data}
-        isLoading={isLoading}
+        trailIds={hike && hike.encoded != '' ? hike.trails : null}
+        hike={hike && hike.encoded == '' ? null : hike}
       ></MapContainer>
 
       <div>Elevation profile</div>
