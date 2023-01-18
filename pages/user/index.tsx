dcba485f29a16c652f97f37c210266ce0a2aa2ac
ThugRaven <kamilo1715@wp.pl>
2023-01-18 16:14:20 +0100

    feat: add user planned hikes list

--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -31,17 +31,69 @@ const User = () => {
     }
   };
 
-  const { isLoading, error, data } = useQuery<any, Error>(
-    ['hikes-count', user?.id],
-    fetchHikesCount,
-    {
-      enabled: true,
-      refetchOnWindowFocus: false,
-      retry: false,
-      onSuccess: (data) => {
-        console.log(data);
-      },
+  const fetchPlannedHikes = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/user/hikes/planned?${new URLSearchParams({
+          page: '1',
+        })}`,
+        {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json' },
+          credentials: 'include',
+        },
+      );
+
+      if (!response.ok) {
+        const data = await response.json();
+        throw new Error(data.message);
+      }
+
+      return response.json();
+    } catch (error) {
+      console.log(error);
+      if (error instanceof Error) {
+        throw new Error(error.message);
+      }
+    }
+  };
+
+  const {
+    isLoading: isLoadingCount,
+    error: errorCount,
+    data: hikesCount,
+  } = useQuery<any, Error>(['hikes-count', user?.id], fetchHikesCount, {
+    enabled: true,
+    refetchOnWindowFocus: false,
+    retry: false,
+    onSuccess: (data) => {
+      console.log(data);
     },
+  });
+
+  const {
+    isLoading: isLoadingPlanned,
+    error: errorPlanned,
+    data: plannedHikes,
+  } = useQuery<any, Error>(['hikes-planned', user?.id], fetchPlannedHikes, {
+    enabled: true,
+    refetchOnWindowFocus: false,
+    retry: false,
+    onSuccess: (data) => {
+      console.log(data);
+    },
+  });
+
+  const plannedHikesList = plannedHikes ? (
+    <ul>
+      {plannedHikes.data.map((hike: any) => (
+        <li key={hike._id}>
+          {hike.name ? `${hike.name.start} - ${hike.name.end}` : hike.query}
+        </li>
+      ))}
+    </ul>
+  ) : (
+    <div>No planned hikes found!</div>
   );
 
   return (
@@ -53,9 +105,10 @@ const User = () => {
             {user?.name}
           </Text>
           <VStack spacing={0} mt={2}>
-            <span>Planned hikes: {data?.plannedHikes}</span>
-            <span>Completed hikes: {data?.completedHikes}</span>
+            <span>Planned hikes: {hikesCount?.plannedHikes}</span>
+            <span>Completed hikes: {hikesCount?.completedHikes}</span>
           </VStack>
+          {plannedHikesList}
         </>
       )}
     </div>
