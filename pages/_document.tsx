eef4ddc7237f96b0800a683ba2634689eb373f94
ThugRaven <kamilo1715@wp.pl>
2023-01-30 20:49:08 +0100

    feat: add `Montserrat` font from google fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,18 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
