f735fe213361ec69db369b018cdeb061e0df2b48
ThugRaven <kamilo1715@wp.pl>
2023-02-02 18:55:18 +0100

    feat: add utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,54 +1,13 @@
-import polyline from '@mapbox/polyline';
-import { ENCODING_PRECISION } from 'constants/constants';
-import { LngLat } from 'mapbox-gl';
+export const formatMinutesToHours = (minutes: number) => {
+  return `${Math.floor(minutes / 60)}:${
+    minutes % 60 >= 10 ? minutes % 60 : `0${minutes % 60}`
+  } h`;
+};
 
-export function createPoint(
-  properties: GeoJSON.GeoJsonProperties,
-  lngLat: LngLat,
-): GeoJSON.Feature<GeoJSON.Point> {
-  return {
-    type: 'Feature',
-    properties: properties,
-    geometry: {
-      type: 'Point',
-      coordinates: [lngLat.lng, lngLat.lat],
-    },
-  };
-}
-
-export function createLineString(
-  properties: GeoJSON.GeoJsonProperties,
-  path: GeoJSON.Position[],
-): GeoJSON.Feature<GeoJSON.LineString> {
-  return {
-    type: 'Feature',
-    properties: properties,
-    geometry: {
-      type: 'LineString',
-      coordinates: path,
-    },
-  };
-}
-
-export function swapCoordinates(
-  coordinates: Array<[number, number]>,
-): Array<[number, number]> {
-  let swapped: Array<[number, number]> = [];
-
-  if (coordinates.length > 0) {
-    for (let i = 0; i < coordinates.length; i++) {
-      const coords = coordinates[i];
-      swapped.push([coords[1], coords[0]]);
-    }
-  }
-
-  return swapped;
-}
-
-export function encode(coordinates: Array<[number, number]>) {
-  return polyline.encode(coordinates, ENCODING_PRECISION);
-}
-
-export function decode(path: string): Array<[number, number]> {
-  return polyline.decode(path, ENCODING_PRECISION);
-}
+export const formatMetersToKm = (meters: number) => {
+  return `${
+    (Math.floor(meters / 1000) * 1000 +
+      Math.round((meters % 1000) / 100) * 100) /
+    1000
+  } km`;
+};
