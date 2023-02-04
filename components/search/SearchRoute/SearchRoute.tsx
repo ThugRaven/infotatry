d52a3f14c0bafe9b027bf71d353946da157366c1
ThugRaven <kamilo1715@wp.pl>
2023-02-04 18:15:22 +0100

    feat: add action buttons to search route

--- components/search/SearchRoute/SearchRoute.tsx
+++ components/search/SearchRoute/SearchRoute.tsx
@@ -3,7 +3,7 @@ import NodeIcon from '@components/icons/NodeIcon';
 import { PopupState } from 'pages/map';
 import { useEffect, useState } from 'react';
 import { FaHiking } from 'react-icons/fa';
-import { MdLandscape } from 'react-icons/md';
+import { MdAdd, MdLandscape, MdSwapVert } from 'react-icons/md';
 import SearchInputTest from '../SearchInputTest';
 import s from './SearchRoute.module.css';
 
@@ -81,81 +81,102 @@ const SearchRoute = ({ onSearch, popupState }: SearchRouteProps) => {
     setSearchForm(newSearchForm);
   };
 
+  const handleReverseRoute = () => {
+    const newSearchForm = [...searchForm].reverse();
+    setSearchForm(newSearchForm);
+  };
+
   return (
-    <form onSubmit={handleSearchRoute} className={s.form}>
-      <SearchInputTest
-        icon={<FaHiking />}
-        searchResults={{ nodes: [], trails: [] }}
-        placeholder="Punkt początkowy..."
-        name="0"
-        value={searchForm[0]}
-        onChange={handleSearchChange}
-        onSearch={() => {
-          console.log('search');
-        }}
-        onClick={() => {
-          console.log('click');
-        }}
-        onRemove={() => handleRemoveDestination(0)}
-      />
+    <form onSubmit={handleSearchRoute}>
+      <div className={s.search}>
+        <SearchInputTest
+          icon={<FaHiking />}
+          searchResults={{ nodes: [], trails: [] }}
+          placeholder="Punkt początkowy..."
+          name="0"
+          value={searchForm[0]}
+          onChange={handleSearchChange}
+          onSearch={() => {
+            console.log('search');
+          }}
+          onClick={() => {
+            console.log('click');
+          }}
+          onRemove={() => handleRemoveDestination(0)}
+        />
 
-      <SearchInputTest
-        icon={searchForm.length <= 2 ? <MdLandscape /> : <NodeIcon />}
-        searchResults={{ nodes: [], trails: [] }}
-        placeholder="Punkt docelowy..."
-        name="1"
-        value={searchForm[1]}
-        onChange={handleSearchChange}
-        onSearch={() => {
-          console.log('search');
-        }}
-        onClick={() => {
-          console.log('click');
-        }}
-        onRemove={() => handleRemoveDestination(1)}
-      />
+        <SearchInputTest
+          icon={searchForm.length <= 2 ? <MdLandscape /> : <NodeIcon />}
+          searchResults={{ nodes: [], trails: [] }}
+          placeholder="Punkt docelowy..."
+          name="1"
+          value={searchForm[1]}
+          onChange={handleSearchChange}
+          onSearch={() => {
+            console.log('search');
+          }}
+          onClick={() => {
+            console.log('click');
+          }}
+          onRemove={() => handleRemoveDestination(1)}
+        />
 
-      {searchForm.length >= 2 && (
-        <>
-          {searchForm.map((value, index) => {
-            if (index > 1) {
-              return (
-                <SearchInputTest
-                  key={index}
-                  icon={
-                    searchForm.length - 1 !== index ? (
-                      <NodeIcon />
-                    ) : (
-                      <MdLandscape />
-                    )
-                  }
-                  searchResults={{ nodes: [], trails: [] }}
-                  placeholder="Punkt docelowy..."
-                  name={index.toString()}
-                  value={searchForm[index]}
-                  onChange={handleSearchChange}
-                  onSearch={() => {
-                    console.log('search');
-                  }}
-                  onClick={() => {
-                    console.log('click');
-                  }}
-                  onRemove={() => handleRemoveDestination(index)}
-                />
-              );
-            }
-          })}
+        {searchForm.length >= 2 && (
+          <>
+            {searchForm.map((value, index) => {
+              if (index > 1) {
+                return (
+                  <SearchInputTest
+                    key={index}
+                    icon={
+                      searchForm.length - 1 !== index ? (
+                        <NodeIcon />
+                      ) : (
+                        <MdLandscape />
+                      )
+                    }
+                    searchResults={{ nodes: [], trails: [] }}
+                    placeholder="Punkt docelowy..."
+                    name={index.toString()}
+                    value={searchForm[index]}
+                    onChange={handleSearchChange}
+                    onSearch={() => {
+                      console.log('search');
+                    }}
+                    onClick={() => {
+                      console.log('click');
+                    }}
+                    onRemove={() => handleRemoveDestination(index)}
+                  />
+                );
+              }
+            })}
+          </>
+        )}
+      </div>
 
-          <Button
-            colorScheme="blue"
-            mb={2}
-            onClick={handleAddDestination}
-            disabled={!searchForm.every((value) => value != '')}
-          >
-            Add destination
-          </Button>
-        </>
-      )}
+      <div className={s.search__actions}>
+        <button
+          className={s.search__action}
+          aria-label="Odwróć trasę"
+          type="button"
+          onClick={handleAddDestination}
+          disabled={!searchForm.every((value) => value != '')}
+        >
+          <MdAdd className={s.search__icon} />
+          <span>Dodaj punkt</span>
+        </button>
+        <button
+          className={s.search__action}
+          aria-label="Dodaj punkt"
+          type="button"
+          onClick={handleReverseRoute}
+        >
+          <MdSwapVert className={s.search__icon} />
+          <span>Odwróć trasę</span>
+        </button>
+      </div>
+
       <Flex direction="column" gap={2}>
         <Button colorScheme="blue" type="submit">
           Search
